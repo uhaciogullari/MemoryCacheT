9faae62f3e980f34e9446277f5f2c1b7c80b7ab8
Ufuk Hacıoğulları <ufukhaciogullari@gmail.com>
2012-09-12 15:26:51 +0300

    Added KeysTests

---
+++ MemoryCacheT.Test/CollectionOperations/KeysTests.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using NUnit.Framework;
+
+namespace MemoryCacheT.Test.CollectionOperations
+{
+    [TestFixture]
+    internal class KeysTests : CacheTestBase
+    {
+        private string _key1;
+        private string _key2;
+        private string _key3;
+        private string[] _keys;
+
+        protected override void FinalizeSetup()
+        {
+            _key1 = "key1";
+            _key2 = "key2";
+            _key3 = "key3";
+            _keys = new[] { _key1, _key2, _key3 };
+        }
+
+        [Test]
+        public void Keys_CacheIsNotEmpty_ChangesAreNotReflected()
+        {
+            _cache.Add(_key1, new NonExpiringCacheItem<int>(_value));
+            _cache.Add(_key2, new NonExpiringCacheItem<int>(_value));
+            _cache.Add(_key3, new NonExpiringCacheItem<int>(_value));
+
+            ICollection<string> keys = _cache.Keys;
+            _cache.Add(_key, new NonExpiringCacheItem<int>(_value));
+
+            bool result = keys.Contains(_key);
+
+            Assert.False(result);
+        }
+
+        [Test]
+        public void Keys_CacheIsNotEmpty_AddingItemsThrowsException()
+        {
+            _cache.Add(_key1, new NonExpiringCacheItem<int>(_value));
+            _cache.Add(_key2, new NonExpiringCacheItem<int>(_value));
+            _cache.Add(_key3, new NonExpiringCacheItem<int>(_value));
+
+            ICollection<string> keys = _cache.Keys;
+
+            Assert.Throws<NotSupportedException>(() => keys.Add(_key));
+        }
+
+        [Test]
+        public void Keys_WhenCacheIsEmpty_ReturnsEmptyCollection()
+        {
+            ICollection<string> keys = _cache.Keys;
+
+            Assert.AreEqual(default(int), keys.Count);
+        }
+    }
+}
