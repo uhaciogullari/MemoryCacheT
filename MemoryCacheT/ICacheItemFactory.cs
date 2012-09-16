f868c6410fd6188ba277059d0eb0431852873c02
Ufuk Hacıoğulları <ufukhaciogullari@gmail.com>
2012-09-16 19:20:34 +0300

    Documentation comments for ICacheItemFactory

--- MemoryCacheT/ICacheItemFactory.cs
+++ MemoryCacheT/ICacheItemFactory.cs
@@ -1,7 +1,16 @@
 ﻿namespace MemoryCacheT
 {
+    /// <summary>
+    /// Represents a factory class that will create cache items.
+    /// </summary>
     public interface ICacheItemFactory
     {
+        /// <summary>
+        /// Creates a new cache item with the provided value.
+        /// </summary>
+        /// <typeparam name="TValue">Type of value in cache item.</typeparam>
+        /// <param name="value">Value of the cache item.</param>
+        /// <returns>Created cache item.</returns>
         ICacheItem<TValue> CreateInstance<TValue>(TValue value);
     }
 }
