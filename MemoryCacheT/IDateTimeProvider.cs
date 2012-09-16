4aa703563ed6d0d58d8bf5a9f23ea66a407323ac
Ufuk Hacıoğulları <ufukhaciogullari@gmail.com>
2012-09-16 19:24:04 +0300

    Documentation comments for IDateTimeProvider

--- MemoryCacheT/IDateTimeProvider.cs
+++ MemoryCacheT/IDateTimeProvider.cs
@@ -2,8 +2,14 @@
 
 namespace MemoryCacheT
 {
+    /// <summary>
+    /// Represents a class that returns current time information.
+    /// </summary>
     public interface IDateTimeProvider
     {
+        /// <summary>
+        /// Returns a DateTime representing the current date and time.
+        /// </summary>
         DateTime Now { get; }
     }
 }
